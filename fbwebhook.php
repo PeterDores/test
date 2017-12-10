cdabbe3d0d375df8ea8c4cb901a331377d0d57e5
PeterDores <34330021+PeterDores@users.noreply.github.com>
2017-12-10 11:14:42 +0600

    Update fbwebhook.php

--- fbwebhook.php
+++ fbwebhook.php
@@ -1,6 +1,6 @@
 <?php
 if(isset($_GET['hub_mode']) && isset($_GET['hub_challenge']) && isset($_GET['hub_verify_token'])){
-    if ($_GET['hub_verify_token']== 'my_verify_token')
+    if ($_GET['hub_verify_token']== 'VERIFY_TOKEN')
         echo $_GET['hub_challenge'];
 }
 http_response_code(200);
